5ce387e07b0b263d7688e3643bb2741895a6abce
Dave Airlie <airlied@redhat.com>
2020-09-01 16:52:52 +1000

    reverse bo movet

--- drivers/gpu/drm/ttm/ttm_bo.c
+++ drivers/gpu/drm/ttm/ttm_bo.c
@@ -315,10 +315,10 @@ static int ttm_bo_handle_move_mem(struct ttm_buffer_object *bo,
 	if (bdev->driver->move_notify)
 		bdev->driver->move_notify(bo, evict, mem);
 
-	if (old_man->use_tt && new_man->use_tt)
-		ret = ttm_bo_move_ttm(bo, ctx, mem);
-	else if (bdev->driver->move)
+	if (bdev->driver->move)
 		ret = bdev->driver->move(bo, evict, ctx, mem);
+	else if (old_man->use_tt && new_man->use_tt)
+		ret = ttm_bo_move_ttm(bo, ctx, mem);
 	else
 		ret = ttm_bo_move_memcpy(bo, ctx, mem);
 
