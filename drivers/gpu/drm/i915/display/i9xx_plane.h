bddaf1354a75f7dfdbc4256f491eee6f00a2ca54
Dave Airlie <airlied@redhat.com>
2020-12-18 16:24:25 +1000

    drm/i915: migrate i9xx plane get config
    Migrate this code out like the skylake code.

--- drivers/gpu/drm/i915/display/i9xx_plane.h
+++ drivers/gpu/drm/i915/display/i9xx_plane.h
@@ -18,4 +18,6 @@ int i9xx_check_plane_surface(struct intel_plane_state *plane_state);
 struct intel_plane *
 intel_primary_plane_create(struct drm_i915_private *dev_priv, enum pipe pipe);
 
+void i9xx_get_initial_plane_config(struct intel_crtc *crtc,
+				   struct intel_initial_plane_config *plane_config);
 #endif
